f9125d4f4902715707a07a0de5394350bb363c97
Axel Ruder <axel.ruder@gmail.com>
2022-09-26 20:30:20 +0200

    added device-name and -id

--- app/espvent_controller.py
+++ app/espvent_controller.py
@@ -1,9 +1,16 @@
 from esp_micro.esp_micro_controller import EspMicroController
 from espvent_device import EspVentDevice
 
+
 class EspVentController(EspMicroController):
     def __init__(self):
         super().__init__()
 
     def createHomieDevice(self, settings):
         return EspVentDevice(settings)
+
+    def getDeviceName(self):
+        return 'espVentuPy'
+
+    def getDeviceID(self):
+        return 'espVentuPy'
