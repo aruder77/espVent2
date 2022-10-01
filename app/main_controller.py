b1974aa57283ae45090b938e7f3e0d2341debd95
Axel Ruder <axel.ruder@gmail.com>
2022-10-01 12:58:53 +0200

    espVentuPy to espVentMicro

--- app/main_controller.py
+++ app/main_controller.py
@@ -1,16 +1,19 @@
+from app.espvent_display import EspVentDisplay
 from esp_micro.esp_micro_controller import EspMicroController
 from espvent_device import EspVentDevice
+from espvent_display import EspVentDisplay
 
 
 class MainController(EspMicroController):
     def __init__(self):
         super().__init__()
+        EspVentDisplay()
 
     def createHomieDevice(self, settings):
         return EspVentDevice(settings)
 
     def getDeviceName(self):
-        return 'espVentuPy'
+        return 'espVentuMicro'
 
     def getDeviceID(self):
-        return 'espVentuPy'
+        return 'espVentuMicro'
